a265fcae6275962930f5d8840f3791abf26cc340
Vioancer <justindanner96@gmail.com>
2024-01-21 17:46:24 +0000

    llama2 requires subcrip

---
+++ falcon.py
@@ -0,0 +1,18 @@
+MODEL_NAME = "vilsonrodrigues/falcon-7b-instruct-sharded"
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
+
+model = AutoModelForCausalLM.from_pretrained(
+    MODEL_NAME,
+    device_map="auto",
+    trust_remote_code=True,
+    quantization_config=bnb_config
+)
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+tokenizer.pad_token = tokenizer.eos_token
